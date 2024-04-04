e481d6b3fe985bcb960a9a73b8d7944512a0ea8e
beicod <beicodcorp@gmail.com>
2024-04-04 18:39:33 -0500

    test

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ counters.forEach(counter => {
   const updateCount = () => {
     const target = +counter.getAttribute('data-target')
     const count = +counter.innerText
-
     const inc = target / speed
 
     if (count < target) {
       counter.innerText = Math.ceil(count + inc)
       setTimeout(updateCount, 1)
     } else {
-      count.innerText = target
+      counter.innerText = target
     }
   }
   updateCount()
