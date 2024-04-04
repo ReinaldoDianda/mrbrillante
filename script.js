805503a0a18687ea47530f1b9b216073321c96f3
beicod <beicodcorp@gmail.com>
2024-04-04 13:17:31 -0500

    soluccion de error footer

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-const counters = document.querySelectorAll('.number-increment-animation');
-const speed = 300;
+const counters = document.querySelectorAll('.number-increment-animation')
+const speed = 300
 
 counters.forEach(counter => {
-    const updateCount = () =>{
-        const target = +counter.getAttribute('data-target')
-        const count = +counter.innerText;
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target')
+    const count = +counter.innerText
 
-        const inc = target / speed;
+    const inc = target / speed
 
-        if( count < target){
-            counter.innerText = Math.ceil(count + inc);
-            setTimeout(updateCount,1);
-        }else{
-            count.innerText = target;
-        }
+    if (count < target) {
+      counter.innerText = Math.ceil(count + inc)
+      setTimeout(updateCount, 1)
+    } else {
+      count.innerText = target
     }
-    updateCount();
-});
+  }
+  updateCount()
+})
 
-
-var acc = document.getElementsByClassName("accordion");
-var i;
+const acc = document.getElementsByClassName('accordion')
+let i
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+    const panel = this.nextElementSibling
+    if (panel.style.display === 'block') {
+      panel.style.display = 'none'
     } else {
-      panel.style.display = "block";
+      panel.style.display = 'block'
     }
-  });
+  })
 }
-
